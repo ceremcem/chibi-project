89ddd71a4a493b3d5fbd82a118b2e2286358466d
ceremcem <ceremcem@ceremcem.net>
2019-06-22 16:32:44 -0400

    improved spi_helpers

--- include/spi_helpers.h
+++ include/spi_helpers.h
@@ -1,6 +1,27 @@
+// taken from:
+// https://github.com/LonelyWolf/stm32/blob/c883e581ed53b1e83eb77bf47d1f4ccb91ce0d25/stm32l4-template/periph/spi.h
 
-// CR2 settings
-// taken from https://github.com/LonelyWolf/stm32/blob/c883e581ed53b1e83eb77bf47d1f4ccb91ce0d25/stm32l4-template/periph/spi.h
+//
+// refer to "Bit definition for SPI_CR1 register" section of MCU header file
+// for more 
+
+
+/* 
+ * CR1 settings 
+ */
+// SPI baud rate prescaler (PCLK/SPI_BR_x)
+#define SPI_BR_2                   ((uint32_t)0x00000000U)                      // 2
+#define SPI_BR_4                   SPI_CR1_BR_0                                 // 4
+#define SPI_BR_8                   SPI_CR1_BR_1                                 // 8
+#define SPI_BR_16                  (SPI_CR1_BR_1 | SPI_CR1_BR_0)                // 16
+#define SPI_BR_32                  SPI_CR1_BR_2                                 // 32
+#define SPI_BR_64                  (SPI_CR1_BR_2 | SPI_CR1_BR_0)                // 64
+#define SPI_BR_128                 (SPI_CR1_BR_2 | SPI_CR1_BR_1)                // 128
+#define SPI_BR_256                 (SPI_CR1_BR_2 | SPI_CR1_BR_1 | SPI_CR1_BR_0) // 256
+
+/*
+ * CR2 settings
+ */
 #define SPI_DW_4BIT                (SPI_CR2_DS_0 | SPI_CR2_DS_1)                               //  4 bits
 #define SPI_DW_5BIT                SPI_CR2_DS_2                                                //  5 bits
 #define SPI_DW_6BIT                (SPI_CR2_DS_2 | SPI_CR2_DS_0)                               //  6 bits
